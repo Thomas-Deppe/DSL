7ddaaca8478fb30566508afd829930303dcd0b90
Thomas-Deppe <56194535+Thomas-Deppe@users.noreply.github.com>
2023-03-18 14:36:49 +0000

    fixed issue with formatting of instrcutions

--- Python Asembler/Assembler.py
+++ Python Asembler/Assembler.py
@@ -225,10 +225,10 @@ def print_to_file(outFile):
     print("-----------Finished writing to a file-----------")
                 
 def compile(): 
-    #inFile = sys.argv[1]
-    #outFile = sys.argv[2]
-    inFile = "basic_instructs.txt"
-    outFile = "ROM_Basic.txt"
+    inFile = sys.argv[1]
+    outFile = sys.argv[2]
+    #inFile = "basic_instructs.txt"
+    #outFile = "ROM_Basic.txt"
     read_instructions(inFile)
     initialise_Instructions()
     convert_instruction_to_mem()
