78d09cea0035ca9759bcaccc5411e9411f13603c
batubayram <95692374+batubayram@users.noreply.github.com>
2023-03-19 13:14:15 +0000

    Minor issue

--- Peripheral_Wrapper_TB.v
+++ Peripheral_Wrapper_TB.v
@@ -1,5 +1,7 @@
 `timescale 1ns / 1ps
 
+
+
 module VGA_TB(
     );
     
@@ -8,25 +10,24 @@ module VGA_TB(
     reg [7:0] BUS_ADDR;          // BUS_ADDR
     reg [7:0] BUS_DATA;          // BUS_DATA decide config colour
     reg BUS_WE;                   // reset button
-    wire VGA_HS;                  
-    wire VGA_VS;                  
+    wire HS;                  
+    wire VS;                  
     wire [7:0] COLOUR_OUT;
     //wire [14:0] FB_ADDR;
     //wire FB_DATA_IN;
-    //wire [15:0] CONFIG_COLOURS;
+    //wire [15:0] CONFIG_COL;
     
-    //Just place your peripheral here
     VGA uut(
             .CLK(CLK),                 
             .RESET(RESET),
             .BUS_ADDR(BUS_ADDR),          
             .BUS_DATA(BUS_DATA),          
             .BUS_WE(BUS_WE),                  
-            .VGA_HS(VGA_HS),                  
-            .VGA_VS(VGA_VS),
+            .HS(HS),                  
+            .VS(VS),
             //.FB_ADDR(FB_ADDR),
             //.FB_DATA_IN(FB_DATA_IN),
-            .CONFIG_COLOURS(CONFIG_COLOURS),        
+            //.CONFIG_COLOURS(CONFIG_COL),        
             .COLOUR_OUT(COLOUR_OUT)
             
     );
@@ -47,12 +48,11 @@ module VGA_TB(
     BUS_WE = 1;
     #40 RESET = 1;
     #30 RESET = 0;
-    #30 BUS_ADDR <= 8'hB0; //0
-    #500 BUS_ADDR <= 8'hB1; //1
-    #500 BUS_ADDR <= 8'hB2; //2
-    #500 BUS_ADDR <= 8'hB0; //1
-    #500 BUS_ADDR <= 8'hB1; //2
-    #500 BUS_ADDR <= 8'hB2; //1
+    forever begin 
+        #200 BUS_ADDR <= 8'hB0; //0
+        #200 BUS_ADDR <= 8'hB1; //1
+        #200 BUS_ADDR <= 8'hB2; //2
+        end
     end
     
     
